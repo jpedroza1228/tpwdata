0c3e460bad1f4b68cb8e53e7121cb7d2ee59bec0
Daniel Anderson <daniela@uoregon.edu>
2021-12-01 14:38:23 -0800

    fix note

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,13 @@
-.onLoad <- function(...) {
+.onAttach <- function(...) {
   key <- Sys.getenv("QUALTRICS_API_KEY")
   if (nchar(key) == 0) {
     packageStartupMessage("Please set your key with `set_key()`")
-  } else {
+  }
+}
+
+.onLoad <- function(...) {
+  key <- Sys.getenv("QUALTRICS_API_KEY")
+  if (nchar(key) > 0) {
     surveys <<- all_surveys()
   }
 }
