75a79b0a32d1839b719660856c25f10e64b88075
Daniel Anderson <daniela@uoregon.edu>
2021-12-02 15:15:44 -0800

    get_survey function

---
+++ R/get_survey.R
@@ -0,0 +1,15 @@
+#' Get a specific survey based on the survey name
+#' @param name The exact name of the survey, passed as a string. If the exact
+#'   name isn't supplied, an error is thrown with the list of all possible
+#'   surveys.
+#' @export
+get_survey <- function(name) {
+  if (!any(name %in% surveys$name)) {
+    stop(
+      "Survey name '", name, "' is not valid. Survey name should be one of\n",
+      paste0(surveys$name, sep = "\n")
+    )
+  }
+  id <- surveys$id[survey$name == name]
+  fetch_survey(surveyID = id)
+}
